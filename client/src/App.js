eee1c6953f38ab6119e4384d9573be01eb0dd3a8
Mantas Astrauskas <mastrauskas4@gmail.com>
2019-10-19 16:56:33 +0000

    Connect Back End with Client and update diary for week 4
    git-svn-id: https://campus.cs.le.ac.uk/svn/ma738/code/branches/master/task-activity-planner@23 1db4ce41-ffac-4b5a-a9c4-010826f1a5c3

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  state = { testItems: [] };
+
+  componentDidMount() {
+    fetch("/api/testItem")
+      .then(res => res.json())
+      .then(testItems => this.setState({ testItems }));
+  }
+
+  render() {
+    const { testItems } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Test Items fetched from Back End and MongoDB</h1>
+        {testItems.map(item => {
+          return <div key={item.id}>{item.name}</div>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
