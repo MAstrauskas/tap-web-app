b84d9ecb361741906bfebc7a8c585c149bb14b9d
Mantas Astrauskas <mastrauskas4@gmail.com>
2020-01-13 05:58:59 +0000

    Add initial Task Suggestion Scenario doc
    git-svn-id: https://campus.cs.le.ac.uk/svn/ma738/code/branches/master/task-activity-planner@86 1db4ce41-ffac-4b5a-a9c4-010826f1a5c3

--- src/resources/Task/helpers/TaskSuggester.js
+++ src/resources/Task/helpers/TaskSuggester.js
@@ -8,6 +8,56 @@ exports.calculateTaskSuggestion = async (email, tasks) => {
     .then(res => (userProductivity = res[0].userProductivity))
     .catch(e => console.log("Cannot find the user. " + e));
 
+  console.log(userProductivity);
   // 2. Calculate based on PRODUCTIVITY, DEALDINES, DIFFICULTY & PRIORITY
-  console.log(tasks);
+  // Idea:
+  // Go through all of the tasks and individually check them by deadline, difficulty and priority
+  // Mark it with one of three: SuggestedOne, SuggestedTwo or SuggestedThree.
+  // Go through all of the tasks again and make `isSuggested` to true starting from SuggestedOne until reach userProductivity limit
 };
+
+/**
+ * Available scenarios:
+ * DEADLINE: Today
+ * DIFFICULTY: Easy
+ * PRIORITY: Low
+ *
+ * DEADLINE: Today
+ * DIFFICULTY: Medium
+ * PRIORITY: Low
+ *
+ * DEADLINE: Today
+ * DIFFICULTY: Hard
+ * PRIORITY: Low
+ *
+ * DEADLINE:  Today
+ * DIFFICULTY: Easy
+ * PRIORITY: Medium
+ *
+ * DEADLINE: Today
+ * DIFFICULTY: Medium
+ * PRIORITY: Medium
+ * Points: 2
+ *
+ * DEADLINE: Today
+ * DIFFICULTY: Hard
+ * PRIORITY: Medium
+ * Points: 2
+ *
+ * DEADLINE: Today
+ * DIFFICULTY: Easy
+ * PRIORITY: High
+ * Points: 1
+ *
+ * DEADLINE: Today
+ * DIFFICULTY: Medium
+ * PRIORITY: High
+ * Points: 3
+ *
+ * DEADLINE: Today
+ * DIFFICULTY: Hard
+ * PRIORITY: High
+ * Points: 4
+ *
+ *
+ */
