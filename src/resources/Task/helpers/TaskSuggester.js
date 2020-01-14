f90ec8643f3edfac8af541418f2e8fdc42ceda89
Mantas Astrauskas <mastrauskas4@gmail.com>
2020-01-14 06:06:10 +0000

    Add 2nd step of the calculation
    git-svn-id: https://campus.cs.le.ac.uk/svn/ma738/code/branches/master/task-activity-planner@87 1db4ce41-ffac-4b5a-a9c4-010826f1a5c3

--- src/resources/Task/helpers/TaskSuggester.js
+++ src/resources/Task/helpers/TaskSuggester.js
@@ -3,61 +3,122 @@ const Users = require("../../User/User.model");
 exports.calculateTaskSuggestion = async (email, tasks) => {
   let userProductivity = 0;
 
-  // 1. Find the user
+  /**
+   * 1. FIND THE USER
+   */
   await Users.find({ email })
     .then(res => (userProductivity = res[0].userProductivity))
     .catch(e => console.log("Cannot find the user. " + e));
 
   console.log(userProductivity);
-  // 2. Calculate based on PRODUCTIVITY, DEALDINES, DIFFICULTY & PRIORITY
   // Idea:
   // Go through all of the tasks and individually check them by deadline, difficulty and priority
-  // Mark it with one of three: SuggestedOne, SuggestedTwo or SuggestedThree.
-  // Go through all of the tasks again and make `isSuggested` to true starting from SuggestedOne until reach userProductivity limit
+  // Add total points to it based on the scenarios doc.
+  // Mark it with one of three: Group 1, Group 2 or Group 3.
+  // Go through all of the tasks again and make `isSuggested` to true starting from Group 1 until reach userProductivity limit
+  console.log(tasks);
+  tasks.map(task => {
+    /**
+     * 2. CALCULATION OF TOTAL POINTS BASED ON DEADLINE, PRIORITY & DIFFICULTY OF THE TASK
+     */
+    let totalPoints = 0;
+    let priorityPoints = 0;
+    let difficultyPoints = 0;
+
+    // Check due date
+    const dueDate = new Date(task.taskDueDate);
+    const todaysDate = new Date();
+    const tomorrowsDate = new Date();
+    const nextWeekDate = new Date();
+
+    tomorrowsDate.setDate(new Date().getDate() + 1);
+    nextWeekDate.setDate(new Date().getDate() + 7);
+
+    const dueDateMidnight = dueDate.setHours(0, 0, 0, 0);
+    const todayMidnight = todaysDate.setHours(0, 0, 0, 0);
+    const tomorrowMidnight = tomorrowsDate.setHours(0, 0, 0, 0);
+    const nextWeekMidnight = nextWeekDate.setHours(0, 0, 0, 0);
+
+    // Today
+    if (dueDateMidnight === todayMidnight) {
+      totalPoints = calculateForDeadline(
+        1,
+        task,
+        totalPoints,
+        priorityPoints,
+        difficultyPoints
+      );
+    }
+
+    // Tomorrow
+    if (dueDateMidnight === tomorrowMidnight) {
+      totalPoints = calculateForDeadline(
+        0.5,
+        task,
+        totalPoints,
+        priorityPoints,
+        difficultyPoints
+      );
+    }
+
+    // Up to next week
+    if (
+      dueDateMidnight > tomorrowMidnight &&
+      dueDateMidnight < nextWeekMidnight
+    ) {
+      totalPoints = calculateForDeadline(
+        0.25,
+        task,
+        totalPoints,
+        priorityPoints,
+        difficultyPoints
+      );
+    }
+
+    console.log("Total PTS: " + totalPoints);
+  });
 };
 
-/**
- * Available scenarios:
- * DEADLINE: Today
- * DIFFICULTY: Easy
- * PRIORITY: Low
- *
- * DEADLINE: Today
- * DIFFICULTY: Medium
- * PRIORITY: Low
- *
- * DEADLINE: Today
- * DIFFICULTY: Hard
- * PRIORITY: Low
- *
- * DEADLINE:  Today
- * DIFFICULTY: Easy
- * PRIORITY: Medium
- *
- * DEADLINE: Today
- * DIFFICULTY: Medium
- * PRIORITY: Medium
- * Points: 2
- *
- * DEADLINE: Today
- * DIFFICULTY: Hard
- * PRIORITY: Medium
- * Points: 2
- *
- * DEADLINE: Today
- * DIFFICULTY: Easy
- * PRIORITY: High
- * Points: 1
- *
- * DEADLINE: Today
- * DIFFICULTY: Medium
- * PRIORITY: High
- * Points: 3
- *
- * DEADLINE: Today
- * DIFFICULTY: Hard
- * PRIORITY: High
- * Points: 4
- *
- *
- */
+function calculateForDeadline(
+  deadlinePoints,
+  task,
+  totalPoints,
+  priorityPoints,
+  difficultyPoints
+) {
+  totalPoints += deadlinePoints;
+
+  priorityPoints += calculatePriority(task.taskPriority, priorityPoints);
+  difficultyPoints += calculateDifficulty(
+    task.taskDifficulty,
+    difficultyPoints
+  );
+
+  totalPoints += priorityPoints + difficultyPoints;
+
+  return totalPoints;
+}
+
+function calculatePriority(priority, totalPoints) {
+  if (priority === "Low") {
+    totalPoints += 0.5;
+  } else if (priority === "Medium") {
+    totalPoints += 1;
+  } else if (priority === "High") {
+    totalPoints += 2;
+  }
+
+  return totalPoints;
+}
+
+function calculateDifficulty(difficulty, totalPoints) {
+  if (difficulty === "Easy") {
+    totalPoints += 0.5;
+  } else if (difficulty === "Medium") {
+    totalPoints += 0.75;
+  } else if (difficulty === "Hard") {
+    totalPoints += 1;
+  }
+
+  return totalPoints;
+}
