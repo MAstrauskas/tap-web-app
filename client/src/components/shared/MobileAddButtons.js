a8c9608aef7dcd58eba4690475114bc6d508b1ff
Mantas Astrauskas <mastrauskas4@gmail.com>
2020-03-12 06:07:44 +0000

    Fix buttons
    git-svn-id: https://campus.cs.le.ac.uk/svn/ma738/code/branches/master/task-activity-planner@177 1db4ce41-ffac-4b5a-a9c4-010826f1a5c3

--- client/src/components/shared/MobileAddButtons.js
+++ client/src/components/shared/MobileAddButtons.js
@@ -3,16 +3,16 @@ import { NavLink } from "react-router-dom";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
 import MoodIcon from "@material-ui/icons/Mood";
-
+import TableFooter from "@material-ui/core/TableFooter";
 export default function MobileAddButtons() {
   return (
-    <div>
+    <TableFooter>
       <NavLink to="/tasks/add">
         <Fab
           color="secondary"
-          size="small"
+          size="medium"
           aria-label="add-task"
-          style={{ position: "absolute", bottom: 2, right: 50 }}
+          style={{ position: "fixed", bottom: "10px", right: 70 }}
         >
           <AddIcon />
         </Fab>
@@ -20,13 +20,13 @@ export default function MobileAddButtons() {
       <NavLink to="/tasks/moodist">
         <Fab
           color="primary"
-          size="small"
+          size="medium"
           aria-label="add-mood"
-          style={{ position: "absolute", bottom: 2, right: 2 }}
+          style={{ position: "fixed", bottom: "10px", right: 10 }}
         >
           <MoodIcon />
         </Fab>
       </NavLink>
-    </div>
+    </TableFooter>
   );
 }
